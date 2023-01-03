3353e61ccd57f9fb404fefa1442378f629338d13
sglegola <sglegola@pzu.pl>
2023-01-03 17:12:38 +0100

    Project created

---
+++ src/main/java/pl/sg/usercatalogue/UserCatalogueApplication.java
@@ -0,0 +1,13 @@
+package pl.sg.usercatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCatalogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sg/usercatalogue/UsercatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sg.usercatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsercatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
