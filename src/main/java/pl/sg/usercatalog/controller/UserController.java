54c6b15abb9df0857962905f313cbb7a0cef0095
Stanislaw Glegola <sta.glegola@gmail.com>
2024-01-11 23:41:30 +0100

    Changed controller methods return type to ResponseEntity to be able to manually set http status

--- src/main/java/pl/sg/usercatalog/controller/UserController.java
+++ src/main/java/pl/sg/usercatalog/controller/UserController.java
@@ -1,6 +1,9 @@
 package pl.sg.usercatalog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -19,7 +22,7 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     @Autowired
     public UserController(UserService userService) {
@@ -27,46 +30,45 @@ public class UserController {
     }
 
     @GetMapping(path = "/all")
-    public List<User> getUsersList() {
-        return userService.getUserList();
+    public ResponseEntity<List<User>> getUsersList() {
+        List<User> userList = userService.getUserList();
+        return new ResponseEntity<>(userList, HttpStatus.OK);
     }
 
     @GetMapping(path = "/{id}")
-    public User getUserById(@PathVariable long id) {
-       return userService.getUserById(id);
+    public ResponseEntity<User> getUserById(@PathVariable long id) {
+        User user = userService.getUserById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @PostMapping(path = "/add")
-    public void addUser(@RequestBody List<User> userList) {
+    public ResponseEntity<Void> addUser(@RequestBody List<User> userList) {
         userService.addUser(userList);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @DeleteMapping(path = "/delete/{id}")
-    public void deleteUserById(@PathVariable("id") long id) {
+    public ResponseEntity<Void> deleteUserById(@PathVariable("id") long id) {
         userService.deleteUserById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PutMapping(path = "/update/{id}")
-    public void updateUser(@PathVariable("id") long id, @RequestBody User updatedUser) {
+    public ResponseEntity<Void> updateUser(@PathVariable("id") long id, @RequestBody User updatedUser) {
         User user = userService.getUserById(id);
-        if (user != null) {
-            user.setUserName(updatedUser.getUserName());
-            user.setAge(updatedUser.getAge());
-            userService.updateUser(user);
-        } else {
-            //TODO Throw exception
-        }
+        user.setUserName(updatedUser.getUserName());
+        user.setAge(updatedUser.getAge());
+        userService.updateUser(user);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PatchMapping(path = "/update-field/{id}")
-    public void updateUserField(@PathVariable("id") long id, @RequestBody User updatedUser) {
-        User user = getUserById(id);
-        if (user != null) {
-            if (updatedUser.getUserName() != null) user.setUserName(updatedUser.getUserName());
-            if (updatedUser.getAge() > 0) user.setAge(updatedUser.getAge());
-            userService.updateUser(user);
-        } else {
-            //TODO Throw exception
-        }
+    public ResponseEntity<Void> updateUserField(@PathVariable("id") long id, @RequestBody User updatedUser) {
+        User user = userService.getUserById(id);
+        if (updatedUser.getUserName() != userService.getUserById(id).getUserName())
+            user.setUserName(updatedUser.getUserName());
+        if (updatedUser.getAge() != userService.getUserById(id).getAge()) user.setAge(updatedUser.getAge());
+        userService.updateUser(user);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
