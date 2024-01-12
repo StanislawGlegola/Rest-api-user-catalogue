468deb91a587a8753bdf1b62f2c7d6b832ce0bb5
Stanislaw Glegola <sta.glegola@gmail.com>
2024-01-12 13:27:51 +0100

    ID value is hidden from most of rest responses except getAll.

--- src/main/java/pl/sg/usercatalog/controller/UserController.java
+++ src/main/java/pl/sg/usercatalog/controller/UserController.java
@@ -1,18 +1,8 @@
 package pl.sg.usercatalog.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.sg.usercatalog.model.User;
 import pl.sg.usercatalog.service.UserService;
 
@@ -24,7 +14,6 @@ public class UserController {
 
     private final UserService userService;
 
-    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
@@ -65,9 +54,10 @@ public class UserController {
     @PatchMapping(path = "/update-field/{id}")
     public ResponseEntity<Void> updateUserField(@PathVariable("id") long id, @RequestBody User updatedUser) {
         User user = userService.getUserById(id);
-        if (updatedUser.getUserName() != userService.getUserById(id).getUserName())
+        if (!updatedUser.getUserName().equals(user.getUserName()))
             user.setUserName(updatedUser.getUserName());
-        if (updatedUser.getAge() != userService.getUserById(id).getAge()) user.setAge(updatedUser.getAge());
+        if (updatedUser.getAge() != user.getAge())
+            user.setAge(updatedUser.getAge());
         userService.updateUser(user);
         return new ResponseEntity<>(HttpStatus.OK);
     }
--- src/main/java/pl/sg/usercatalog/model/User.java
+++ src/main/java/pl/sg/usercatalog/model/User.java
@@ -1,11 +1,13 @@
 package pl.sg.usercatalog.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class User {
+    @ApiModelProperty(hidden = true)
     private long id;
     private String userName;
     private int age;
