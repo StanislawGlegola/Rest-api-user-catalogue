82d4322a95e9c9d3e7f40caf223d179a32f14f4a
Stanislaw Glegola <sta.glegola@gmail.com>
2024-01-21 13:06:26 +0100

    User DAO and DTO introduced, mapper introduced

---
+++ src/main/java/pl/sg/usercatalog/model/UserDTO.java
@@ -0,0 +1,33 @@
+package pl.sg.usercatalog.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@RedisHash("User")
+public class UserDAO {
+    private long id;
+    private String userName;
+    private int age;
+    private Gender gender;
+    private String email;
+    private String description;
+    private LocalDateTime registrationDate;
+    private LocalDateTime modificationDate;
+    private boolean modified;
+
+    public UserDAO(String userName, int age, String email, String description, LocalDateTime registrationDate, LocalDateTime modificationDate, boolean modified, Gender gender) {
+        this.userName = userName;
+        this.age = age;
+        this.email = email;
+        this.description = description;
+        this.registrationDate = registrationDate;
+        this.modificationDate = modificationDate;
+        this.modified = modified;
+        this.gender = gender;
+    }
+}
