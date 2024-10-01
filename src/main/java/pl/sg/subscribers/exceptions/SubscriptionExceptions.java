135f9f5d49e4064c721286e392e3ecdcdccc515e
Stanislaw Glegola <sta.glegola@gmail.com>
2024-10-02 00:36:27 +0200

    Cleaning master from artifacts

--- src/main/java/pl/sg/subscribers/exceptions/SubscriptionExceptions.java
+++
@@ -1,7 +0,0 @@
-package pl.sg.subscribers.exceptions;
-
-public class SubscriptionExceptions extends RuntimeException {
-    public SubscriptionExceptions(String message) {
-        super(message);
-    }
-}
--- src/main/java/pl/sg/users/model/Gender.java
+++
@@ -1,5 +0,0 @@
-package pl.sg.usercatalog.model;
-
-public enum Gender {
-    MALE, FEMALE
-}
